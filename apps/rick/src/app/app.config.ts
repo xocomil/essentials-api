c095f986d01bc88be5b6390ef3e3c4b4f1a1eb24
xocomil <xocomil@gmail.com>
2023-05-07 09:53:11 -0600

    upgrade `nx`

---
+++ apps/rick/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+} from '@angular/router';
+import { provideApiBase } from '@rick/api';
+import { appRoutes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideApiBase(),
+    provideHttpClient(),
+    provideAnimations(),
+  ],
+};
