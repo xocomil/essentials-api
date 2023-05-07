b2444763002b949b575cbf752ef0f1be3fa1295f
xocomil <xocomil@gmail.com>
2023-05-07 09:52:52 -0600

    upgrade `nx` and `angular`

--- apps/rick/src/main.ts
+++ apps/rick/src/main.ts
@@ -1,19 +1,9 @@
-import { provideHttpClient } from '@angular/common/http';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from '@angular/router';
-import { provideApiBase } from '@rick/api';
-import { AppComponent } from './app/app.component';
-import { appRoutes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-    provideApiBase(),
-    provideHttpClient(),
-    provideAnimations(),
-  ],
-}).catch((err) => console.error(err));
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
