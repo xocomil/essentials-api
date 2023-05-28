4b5d3d3d2039cd485f4fcb98f50e2e40f38e8200
xocomil <xocomil@gmail.com>
2023-05-28 12:48:09 -0600

    added ability to page through episodes

--- libs/episodes/src/lib/episodes.routes.ts
+++ libs/episodes/src/lib/episodes.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 
 export const episodeRoutes: Routes = [
   {
-    path: '',
-    loadComponent: () =>
-      import('./episodes/episodes.component').then((c) => c.EpisodesComponent),
-  },
-  {
     path: ':id',
     loadComponent: () =>
       import('./episode/episode.component').then((c) => c.EpisodeComponent),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./episodes/episodes.component').then((c) => c.EpisodesComponent),
   },
   {
     path: '**',
